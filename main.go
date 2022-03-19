4cef8abd00d2663929628986d3d3de6f6da2504c
Henry Finucane <h.finucane@gmail.com>
2022-03-19 15:34:02 -0700

    Do everything in parallel, with a deadline
    I'm surprised at how much slower this made the actual work- locking is
    rough

--- main.go
+++ main.go
@@ -9,7 +9,31 @@ import (
 	"time"
 )
 
-func Lookup(ctx context.Context, hostname, dns_server string) ([]string, error) {
+// Heritage Nagios Return Codes
+const (
+	OK = iota
+	WARNING
+	CRITICAL
+	UNKNOWN
+)
+
+type LookupResult struct {
+	Results []string
+	Server  string
+	Time    time.Duration
+	Error   error
+}
+
+func (lr *LookupResult) Print() {
+	if lr.Error == nil {
+		fmt.Println(lr.Server, lr.Results, "in", lr.Time)
+	} else {
+		fmt.Println("Failed:", lr.Error)
+	}
+}
+
+func Lookup(ctx context.Context, hostname, dns_server string) LookupResult {
+	start := time.Now()
 	r := &net.Resolver{
 		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
 			d := net.Dialer{
@@ -19,11 +43,18 @@ func Lookup(ctx context.Context, hostname, dns_server string) ([]string, error) 
 		},
 	}
 
-	return r.LookupHost(ctx, hostname)
+	results, err := r.LookupHost(ctx, hostname)
+	return LookupResult{
+		Server:  dns_server,
+		Results: results,
+		Time:    time.Since(start),
+		Error:   err,
+	}
 }
 
 func main() {
 	hostname := flag.String("host", "", "host name to look up")
+	full_deadline := time.Second * 10
 	flag.Parse()
 
 	if *hostname == "" {
@@ -34,12 +65,29 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*1))
 	defer cancel()
 
+	results := make(chan LookupResult, len(flag.Args()))
+
 	for _, dns_server := range flag.Args() {
-		strings, err := Lookup(ctx, *hostname, fmt.Sprintf("%s:53", dns_server))
-		fmt.Println(strings)
-		fmt.Println(err)
-		if err != nil {
-			os.Exit(2)
+		go func() {
+			results <- Lookup(ctx, *hostname, fmt.Sprintf("%s:53", dns_server))
+		}()
+	}
+
+	errors := 0
+	for i := 0; i < len(flag.Args()); i++ {
+		select {
+		case lookup := <-results:
+			lookup.Print()
+			if lookup.Error != nil {
+				errors++
+			}
+		case <-time.After(full_deadline):
+			break
 		}
 	}
+
+	if errors > 0 {
+		os.Exit(CRITICAL)
+	}
+	os.Exit(OK)
 }
